83b597e96f06894fac05cb22b8153ee4c4ca6cea
Bert Van den Broeck <bert.vandenbroeck@student.odisee.be>
2022-12-06 16:39:23 +0100

    Day 6

---
+++ Day 6/1.py
@@ -0,0 +1,17 @@
+f = open('Day 6/input.txt')
+
+letter_list = list(f.readline())
+four_letter_list = []
+
+for i in range(0, len(letter_list)):
+    if len(four_letter_list) < 4:
+        four_letter_list.append(letter_list[i])
+    elif len(set(four_letter_list)) == len(four_letter_list):
+        end = i
+        break
+    else:
+        four_letter_list.pop(0)
+        four_letter_list.append(letter_list[i])
+
+
+print(end)
